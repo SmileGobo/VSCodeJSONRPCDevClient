558a05790136f3e60800c06b91fc7fc12320de42
Копысов Евгений <kopysov@bolid.ru>
2020-09-04 18:17:18 +0300

    Разботал как использовать xpath

--- core/ResourcePather.js
+++ core/ResourcePather.js
@@ -1,38 +1,67 @@
-const xpath  = require('xpath');
+const XPATH  = require('xpath');
 const path   = require('path');
 const xmldom = require('xmldom');
-
+const ASS    = require('assert'); 
+const { assert } = require('console');
 module.exports =  class {
-    
+    //TODO ResourceLocator
+    //addFilter({tag, attr});
+    //removeFilter()
+    //clear()
     set html( html_data){
-        this._html_doc = new xmldom.DOMParser().parseFromString(html_data);
+        html_data = html_data.trim();
+        ASS.ok(!!html_data, 'require_not empty string');
+
+        this._html_doc = this._parser.parseFromString(html_data, 'text/html');
+        //console.log(this._html_doc);
     }
 
     get html() {
+        ASS.notEqual(this._html_doc, null);
         let srlzr = new xmldom.XMLSerializer();
         return srlzr.serializeToString(this._html_doc);
-        //this._html_doc.
     }
 	set basePath(value){
-		this._base_path = value;
+        ASS.ok(path.isAbsolute(value), 'Absolute path required');
+        let data = path.parse(value);
+        ASS.ok(!!data.dir, 'empty value not allow');
+		this._base_path = value.trim();
 	}
 
 	_base_path = '';
     _html_doc = null;
+    _parser   = new xmldom.DOMParser({
+        errorHandler: (_, msg) => {
+            this._html_doc = null;
+            throw msg;
+        }
+    });
     
     fixResourceReferences() {
-        const link_attrs = ['href', 'src'];
-        const modify_tags = Set(['link', 'script', 'import']);
+        ASS.notEqual(this._html_doc, null, 'html not setup');
+        ASS.notEqual(this._base_path, '', 'basePath not setup');
 
-        link_attrs.forEach(attr => {
-            const xpath = `.//*[${attr}]`; 
-            let nodes = xpath.evaluate(xpath, this._html_doc);
+        const rqsts = [
+            {tag: 'link',   attr: 'href'},
+            {tag: 'script', attr: 'src'}
+        ];
+        //костыль благодоря котрому можно делать запросы  вида
+        // '//x:<tag>...'
+        //https://stackoverflow.com/questions/25753368/performant-parsing-of-pages-with-node-js-and-xpath
+        const select = XPATH.useNamespaces({"x": "http://www.w3.org/1999/xhtml"});
+        rqsts.forEach(rqst => {
+            let xpath = `//x:${rqst.tag}[@${rqst.attr}]`;
+            let nodes = select(xpath, this._html_doc);
             nodes.forEach( node => {
-                if (modify_tags.has(node.tagName)){
-                    let link_path = node.getAttribute(attr);
-                    link_path = `vscode-resource://${path.join(this._base_path, link_path)}`;
-                    node.setAttribute('href', link_path);
+                const type = node.getAttribute('rel') ?? '';
+                if (type === 'import' ){
+                    //TODO вгрузку веб компонентов
+                    return; 
                 }
+                let link_path = node.getAttribute(rqst.attr);
+                //TODO скорректировать под винду
+                link_path = `vscode-resource:/${path.join(this._base_path, link_path)}`;
+                node.setAttribute(rqst.attr, link_path);
             });
         });
     }
--- test/ResourcePather.js
+++ test/ResourcePather.js
@@ -1,10 +1,75 @@
 const ResourcePather = require ('../core/ResourcePather');
-
+const assert  = require('assert');
+const path    = require('path');
 suite('ResourcePather', () => {
-    let rp = new ResourcePather();
-    test('html', ()=> {
+    let rp = null;
+    setup(() => rp = new ResourcePather());
+
+    test('setup html', () => {
+        const html = '<html><head></head><body><body></html>';
         assert.doesNotThrow( () => {
-            rp.html = '';
+            rp.html = html;
+        }, 'trivial html data');
+
+        assert.throws(() => {
+            rp.html = ' ';
+        }, 'check empty string');
+
+        assert.throws(() => {
+            rp.html = '<html><head</html>';
+        }, 'wrong xml');
+    });
+
+    test('set base path', () => {
+        assert.throws(() => rp.basePath = '', 'not empty path' );
+        assert.doesNotThrow(() => rp.basePath = path.join(path.sep, 'test', 'test'));
+        assert.doesNotThrow(() => rp.basePath = path.join(path.sep, 'test'));
+    });
+
+    test('fixResourceReferences setup', () => {
+        assert.throws(() => rp.fixResourceReferences(), 'not setup');
+        
+        assert.doesNotThrow(() => rp.html = '<html></html>');
+        assert.throws(() => rp.fixResourceReferences(), 'already not setup');
+        assert.doesNotThrow(() => rp.basePath = path.join(path.sep, 'test', 'hello'));
+        assert.doesNotThrow(() => rp.fixResourceReferences(), 'success setup');
+    });
+    const data = `<html>
+                <head>
+                    <link rel="stylesheet" href="UI/css/main.css">
+                    <link rel="import" href="UI/components/test.html">
+                    <script type="module" src="./test.js"></script>
+                </head>
+                <body>
+                    <div id="app"></div>
+                </body>
+            </html>`;
+
+    test('XPATH', () => {
+        const XPATH  = require('xpath');
+        const XML    = require('xmldom');
+        const select = XPATH.useNamespaces({"x": "http://www.w3.org/1999/xhtml"})
+        let parser = new XML.DOMParser({errorHandler: (lvl, err) => {throw err}});
+        let root   = parser.parseFromString(data, 'text/html'); 
+        let links =  select('//@href', root);
+        assert.equal(links.length, 2);
+
+        links = select('//x:link[@href]', root);
+        assert.equal(links.length, 2);
+    });
+    test('fixResourceRefrences result', () => {
+        
+        
+        assert.doesNotThrow(() => {
+            rp.html     = data;
+            rp.basePath = path.join(path.sep, 'home', '.local', 'share', 'plugins');
+            rp.fixResourceReferences();
         });
+        let rslt = '';
+        assert.doesNotThrow(() => rslt = rp.html, 'read result');
+        assert.notEqual(rslt.length, 0, 'result not empty');
+        const count = (rslt.match(/vscode-resource/g) || []).length;
+        assert.equal(count, 2, 'path success 2 nodes modified');
+        
     });
 });
