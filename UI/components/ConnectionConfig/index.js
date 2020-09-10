381a953b2083f327c72eca68218d771921d9e685
Копысов Евгений <kopysov@bolid.ru>
2020-09-10 20:26:33 +0300

    Внедрид хелпер для кнопки и списка, и колбек для управления поведением формы.

--- UI/components/ConnectionConfig/index.js
+++ UI/components/ConnectionConfig/index.js
@@ -1,8 +1,81 @@
+function isFunction(obj) {
+    return !!(obj && obj.constructor && obj.call && obj.apply);
+};
+class ToggleButton {
+    _state = false;
+    _caption = {
+        false: 'Connect',
+        true: 'Disconnect' 
+    }
+    _clbck = null;
+
+    _widjet = null;
+    constructor(widjet){
+        this._widjet = widjet;
+        this._setCaption();
+        this._widjet.onclick = this.toggle.bind(this);
+    }
+    toggle(){
+        let update = !this._state;
+        let success = () => {
+            this._state = update;
+            this._setCaption();
+        }
+        if (this._clbck){
+            this._clbck(update).resolve(success)
+        }
+        else {
+            success();
+        }
+    }
+    _setCaption(){
+        this._widjet.setAttribute('value', this._caption[this._state]);
+    }
+
+    set onToggle(val) {
+        if (!isFunction(val)){
+            throw TypeError('value must be function');
+        }
+        this._clbck = val;
+    }
+    reset(){
+        this._state = false;
+        this._setCaption;
+    }
+
+    set disabled(val){
+        if (val){
+            this._widjet.setAttribute('disabled', true);
+        }
+        else {
+            this._widjet.removeAttribute('disbaled');
+        }
+    }
+}
+class SelectHelper{
+    constructor(widjet){
+        this._widjet = widjet;
+        
+    }
+
+    get value(){
+        return this._widjet.options[this._widjet.selectedIndex].value;
+    } 
+
+    set onChange(clbck) {
+        this._widjet.onchange =  evt => {
+            clbck(this);
+        };
+    }
+    
+
+}
 customElements.define('connection-config', class LogInteraction extends HTMLElement {
     TEMPLATE_ID = '#connection-config';
     constructor() {
         super();
     }
+
     connectedCallback() {
         let shadow = this.attachShadow({mode: 'open'});
         this._render(shadow);
@@ -12,12 +85,25 @@ customElements.define('connection-config', class LogInteraction extends HTMLElem
     }
 
     _afterRender(shadow) {
+        let form = shadow.querySelector('form').elements
         this._widjet = {
             title: shadow.querySelector('h2'),
-            form:  shadow.querySelector('form').elements
+            form,
+            button: new ToggleButton(form.submit),
+            select: new SelectHelper(form.proto)
         }
         this._widjet.title.innerText = this.getAttribute('title') || '';
-        this._widjet.form.submit.onclick = this.submit.bind(this);
+        //this._widjet.form.submit.onclick = this.submit.bind(this);
+        this._widjet.select.onChange = (w) => {
+            let label = this._widjet.form.session.parentElement;
+            if (w.value === 'TCP'){
+                label.classList.remove('hidden');
+            }
+            else {
+                label.classList.add('hidden');
+            }
+            
+        }
     }
 
     _render(shadow){
