80a3c2f4e24d984832d1f2dad978602695cf5911
Копысов Евгений <kopysov@bolid.ru>
2020-09-14 21:18:40 +0300

    Компонент таблицы добавление строк

--- UI/components/RPCRequestList/index.js
+++ UI/components/RPCRequestList/index.js
@@ -1,3 +1,34 @@
+
+class RequestItem {
+    TEMPLATE_ID = '#request-list-item';
+    constructor(row){
+        let tmpl = document.querySelector(this.TEMPLATE_ID);
+        row.appendChild(tmpl.content.cloneNode(true));
+        let cells = row.querySelectorAll('td'); 
+        this._widjet = {
+            num   : cells[0],
+            method: cells[1],
+            params: cells[2],
+            send:   row.querySelector('input[name=send]')
+        };
+    }
+
+    set num(val){
+        this._widjet.num.innerText = val;
+    }
+
+    set method(val) {
+        this._widjet.method.innerText = val;
+    }
+
+    set params(val) {
+        this._widjet.params.innerText = JSON.stringify(val);
+    }
+
+    set onSend(action) {
+        this._widjet.send.onclick = action;
+    }
+}
 class RequestList extends HTMLElement {
     TEMPLATE_ID = '#request-list';
     CHILD_NAME  = 'rpc-request';
@@ -14,14 +45,25 @@ class RequestList extends HTMLElement {
     }
 
     _afterRender(shadow) {
-        this._widjet = {
-          
-        }
+        this._widjet = shadow.querySelector('table');
     }
 
     _render(shadow){
         let tmpl = document.querySelector(this.TEMPLATE_ID);
         shadow.appendChild(tmpl.content.cloneNode(true));
+    }
+
+    addItem(value){
+        let row = new RequestItem(this._widjet.insertRow());
+        row.num    = this._records.length;
+        row.method = value.method;
+        row.params = value.params;
+        this._records.push(value);
+        row.onSend = this._onSend.bind(this, value);
+    }
+
+    _onSend(val){
+        console.log(`send: ${val}`);
     }
 }
 
