6c216dadb083fd6e39e3259079e3caf0b19edd71
Копысов Евгений <kopysov@bolid.ru>
2020-09-18 12:38:48 +0300

    Удален не используемый код

--- extension.js
+++
@@ -1,8 +0,0 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-
-
-const Plugin = require('./main');
-
-
-module.exports = new Plugin();
